463700c149f38b7d85ec023a3284632c1587f6d2
robertsteele7 <atpcycle7@gmail.com>
2022-10-09 16:51:56 -0500

    git@github.com:robertsteele7/Static-Exercise.git

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"celsius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
         }
     }
 }
